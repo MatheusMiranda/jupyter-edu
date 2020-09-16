acfd7e21c507c02727c3bc2a4774cfce7fbea72a
Matheus Miranda <matheusmirandalacerda@gmail.com>
2020-09-15 21:52:24 -0300

    Enable connection to postgres
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -19,7 +19,7 @@ c.DockerSpawner.container_image = os.environ['DOCKER_NOTEBOOK_IMAGE']
 # jupyter/docker-stacks *-notebook images as the Docker run command when
 # spawning containers.  Optionally, you can override the Docker run command
 # using the DOCKER_SPAWN_CMD environment variable.
-spawn_cmd = os.environ.get('DOCKER_SPAWN_CMD', "start-singleuser.sh")
+spawn_cmd = 'jupyter-lab'
 c.DockerSpawner.extra_create_kwargs.update({ 'command': spawn_cmd })
 # Connect containers to this Docker network
 network_name = os.environ['DOCKER_NETWORK_NAME']
@@ -48,22 +48,26 @@ c.JupyterHub.hub_ip = 'jupyterhub'
 c.JupyterHub.hub_port = 8080
 
 # TLS config
-c.JupyterHub.port = 443
-c.JupyterHub.ssl_key = os.environ['SSL_KEY']
-c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
+#c.JupyterHub.port = 443
+#c.JupyterHub.ssl_key = os.environ['SSL_KEY']
+#c.JupyterHub.ssl_cert = os.environ['SSL_CERT']
 
 # Authenticate users with GitHub OAuth
 #c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
 #c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
-c.DummyAuthenticator.password = "some_password"
+
+# Dummy Authenticator - ONLY USE IT FOR TESTING ENVIRONMENTS
+c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+c.DummyAuthenticator.password = "teste123"
 
 # Persist hub data on volume mounted inside container
 data_dir = os.environ.get('DATA_VOLUME_CONTAINER', '/data')
 
-c.JupyterHub.cookie_secret_file = os.path.join(data_dir,
-    'jupyterhub_cookie_secret')
+#c.JupyterHub.cookie_secret_file = os.path.join(data_dir,
+#    'jupyterhub_cookie_secret')
 
-c.JupyterHub.db_url = 'postgresql://postgres:{password}@{host}/{db}'.format(
+c.JupyterHub.db_url = 'postgresql://{user}:{password}@{host}/{db}'.format(
+    user=os.environ['POSTGRES_USER'],
     host=os.environ['POSTGRES_HOST'],
     password=os.environ['POSTGRES_PASSWORD'],
     db=os.environ['POSTGRES_DB'],
